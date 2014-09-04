f0f14521444ef2f11e0fdfb003d9753c8c117180
Andrew Gallant <jamslam@gmail.com>
2014-09-04 14:53:12 -0400

    Hacky fix for when 'mapping' is not present.
    I have no idea whether this is the right thing to do or not, but it
    seems to work.

--- rest_framework_nested/routers.py
+++ rest_framework_nested/routers.py
@@ -69,6 +69,8 @@ class NestedSimpleRouter(SimpleRouter):
             route_contents = route._asdict()
 
             route_contents['url'] = route.url.replace('^', '^'+self.parent_regex)
+            if 'mapping' not in route_contents:
+                route_contents['mapping'] = {}
             nested_routes.append(rest_framework.routers.Route(**route_contents))
 
         self.routes = nested_routes
