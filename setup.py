e876ed43f981b46194ceb17145dfdf8c777074e0
Micah Denbraver <macromicah@gmail.com>
2014-10-18 22:12:16 -0700

    Added test command to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
 import os
+import sys
 
 from setuptools import find_packages, setup
+from setuptools.command.test import test
 
 curdir = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestCommand(test):
+    user_options = []
+
+    def initialize_options(self):
+        test.initialize_options(self)
+
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        from rest_framework_nested.runtests import runtests
+        errno = runtests.main()
+        sys.exit(errno)
 
 setup(
     name='drf-nested-routers',
@@ -17,4 +37,6 @@ setup(
     install_requires=['djangorestframework'],
     setup_requires=['setuptools'],
     packages=find_packages(curdir),
+    tests_require=['djangorestframework<2.4', 'Django<1.7'],
+    cmdclass={'test': TestCommand},
 )
