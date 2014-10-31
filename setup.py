7ab62a33e7c734b1d270978a0c539a5176e2f45c
Alan Justino <alan.justino@yahoo.com.br>
2014-10-31 14:13:50 -0200

    Version bump, only for DRF >= 2.4

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     description='Nested resources for the Django Rest Framework',
     long_description=open('README.md').read(),
     license='Apache',
-    version='0.1.3',
+    version='0.9.0',
     author='Alan Justino and Oscar Vilaplana',
     author_email='alan.justino@yahoo.com.br, dev@oscarvilaplana.cat',
     url='https://github.com/alanjds/drf-nested-routers',
-    install_requires=['djangorestframework'],
+    install_requires=['djangorestframework>=2.4'],
     setup_requires=['setuptools'],
     packages=find_packages(curdir),
 )
