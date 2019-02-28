c2d3c2c471dfb504626509a34256eb2d9898cfa2
Pudding <s.kokuryo@gmail.com>
2019-03-01 00:20:32 +0900

    Fix to use get_serializer_class method instead of serializer_class

--- rest_framework_nested/viewsets.py
+++ rest_framework_nested/viewsets.py
@@ -5,9 +5,10 @@ class NestedViewSetMixin(object):
         `serializer_class.parent_lookup_kwargs`.
         """
         queryset = super(NestedViewSetMixin, self).get_queryset()
-        if hasattr(self.serializer_class, 'parent_lookup_kwargs'):
+        serializer_class = self.get_serializer_class()
+        if hasattr(serializer_class, 'parent_lookup_kwargs'):
             orm_filters = {}
-            for query_param, field_name in self.serializer_class.parent_lookup_kwargs.items():
+            for query_param, field_name in serializer_class.parent_lookup_kwargs.items():
                 orm_filters[field_name] = self.kwargs[query_param]
             return queryset.filter(**orm_filters)
         return queryset
