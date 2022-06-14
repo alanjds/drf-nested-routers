6d3b6b016a36f0b9252325dcdf371a3bff811452
Андрей Шайкин <kiwibon@yandex.ru>
2022-06-14 20:53:35 +0300

    add support django 4

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
