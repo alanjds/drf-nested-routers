e2a6933521cf99ed0a2d0faae383a92e9f8ff419
Klaas van Schelven <klaas@vanschelven.com>
2025-09-08 17:49:31 +0200

    More completely reference Django 5.2 and DRF 3.16 compat

--- setup.py
+++ setup.py
@@ -86,6 +86,8 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
